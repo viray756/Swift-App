11e17ed6ce0b5d672ff1153fffb9173232af6dbd
Jose Viray <Angelo@joses-air.home>
2017-01-14 14:40:31 -0500

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Jose Viray on 1/14/17.
+//  Copyright © 2017 Jose Viray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

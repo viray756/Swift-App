c99607ea03a823ce0f3ccc72186656e58ad7443b
Jose Viray <viray756@gmail.com>
2017-01-18 21:24:55 -0500

    Playground: Arrays

--- Swift App/TableView Playground.playground/Contents.swift
+++ Swift App/TableView Playground.playground/Contents.swift
@@ -1,1 +1,14 @@
 
+var favCandy = ["100 Grand","Crunch","Kit Kat"]
+
+favCandy.append("Hershey's")
+favCandy
+
+var favNumbers = [1,2,3,4,5]
+
+favNumbers.append(22)
+favNumbers.count
+
+favNumbers[0] = 3
+favNumbers
+favNumbers.insert(99, at: 5)

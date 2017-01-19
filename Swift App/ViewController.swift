d7129b17a99d3712baf7417885163fbd4295dd53
Jose Viray <viray756@gmail.com>
2017-01-18 19:14:28 -0500

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -3,16 +3,31 @@
 //  Swift App
 //
 //  Created by Jose Viray on 1/14/17.
-//  Copyright © 2017 Jose Viray. All rights reserved.
+//  Copyright © 2017 Jose Viray. All rights reserved...
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TheLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount == 3 {
+            TheLabel.text = "You have tapped this 3 times!"
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {

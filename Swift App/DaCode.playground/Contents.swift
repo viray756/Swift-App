76e81052ad3f5eea130497bd838fe5af35595e4b
Jose Viray <viray756@gmail.com>
2017-01-18 20:30:30 -0500

    Playground Code

---
+++ Swift App/DaCode.playground/Contents.swift
@@ -0,0 +1,37 @@
+
+import Foundation
+var age = 12
+var candy = "100 Grand Bar"
+
+if age != 16 {
+    print("You Can go On the Ride!")
+}
+
+var name = "Bo"
+
+if name == "Angelo" {
+    print("We have the same name")
+} else {
+    print("We dont have the same name :(")
+}
+
+5+8
+5.5/1
+5.5+1
+
+var weight = 180.5
+
+age*Int(weight)
+ sqrt(25)
+pow(25.0,0.5)
+
+var agey = "27.0"
+Double(agey)
+
+
+var coolText = ""
+
+coolText = "Answer is... \(Double("5")!+Double("78")!)"
+
+"My age is... \(age)"
+
